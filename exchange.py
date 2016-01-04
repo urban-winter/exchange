17cdaca165b9b1e2eeccc41436828313ac82aba9
Piers <piers@davenport-thompson.org.uk>
2016-01-04 22:30:46 +0000

    matches removed from order book

--- exchange.py
+++ exchange.py
@@ -34,6 +34,10 @@ class Exchange(object):
             for sell_order in self._sell_order_book:
                 if buy_order.quantity == sell_order.quantity:
                     trades.append(Trade(buy=buy_order, sell=sell_order))
+                    break
+        for trade in trades:
+            self._buy_order_book.remove(trade.buy)
+            self._sell_order_book.remove(trade.sell)
         return trades
 
 class TestExchange(unittest.TestCase):
@@ -50,12 +54,7 @@ class TestExchange(unittest.TestCase):
         exchange = Exchange()
         matches = exchange.match_orders()
         self.assertEqual(matches, [])
-        
-# orders will match if there is a buy and sell order with same amount
-# multiple sells, single buy
-# multiple buys, single sell
-# volumes don't match
-        
+
     def test_match_orders(self):
         exchange = Exchange()
         buy_order = Order('buy',1000)
@@ -66,6 +65,22 @@ class TestExchange(unittest.TestCase):
         self.assertEqual(len(trades), 1)
         self.assertEqual(trades[0].buy, buy_order)
         self.assertEqual(trades[0].sell, sell_order)
+        
+    def test_matched_orders_removed_from_order_book(self):
+        exchange = Exchange()
+        buy_order = Order('buy',1000)
+        sell_order = Order('sell',1000)
+        exchange.submit_order(buy_order)
+        exchange.submit_order(sell_order)
+        exchange.match_orders()
+        orders = exchange.order_book()
+        self.assertEqual(orders, [])
+
+# orders will match if there is a buy and sell order with same amount
+# multiple sells, single buy
+# multiple buys, single sell
+# volumes don't match
+        
 
 # check that orders are removed
 # check that match price is reported
