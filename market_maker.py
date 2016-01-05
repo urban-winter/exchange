07c69cd4c6767d64f95360c8a309df9f9ba5ebb5
Piers <piers@davenport-thompson.org.uk>
2016-01-05 22:40:02 +0000

    market maker volume test

--- market_maker.py
+++ market_maker.py
@@ -12,14 +12,15 @@ from exchange import Exchange, Order
 
 class MarketMaker(object):
     MARGIN = 0.02
+    ORDER_QUANTITY = 100
     def __init__(self, exchange):
         self.exchange = exchange
 
     def trade(self):
         offer_price = self.exchange.last_trade()[0] * (1 - self.MARGIN/2)
         bid_price = self.exchange.last_trade()[0] * (1 + self.MARGIN/2)
-        self.exchange.submit_order(Order('buy',123,offer_price))
-        self.exchange.submit_order(Order('sell',456,bid_price))
+        self.exchange.submit_order(Order('buy',self.ORDER_QUANTITY,offer_price))
+        self.exchange.submit_order(Order('sell',self.ORDER_QUANTITY,bid_price))
 
 class TestMarketMaker(unittest.TestCase):
 
@@ -27,13 +28,13 @@ class TestMarketMaker(unittest.TestCase):
         exchange = Exchange()
         mm = MarketMaker(exchange)
         mm.trade()
-        self.assertEqual(len(exchange.order_book()), 2)
-        # check order prices
+        # check order prices and volumes
         expected_offer = exchange.OPEN_DEFAULT_PRICE * (1 + mm.MARGIN/2)
         expected_bid = exchange.OPEN_DEFAULT_PRICE * (1 - mm.MARGIN/2)
         self.assertEqual(exchange.buy_order_book()[0].price, expected_bid)
         self.assertEqual(exchange.sell_order_book()[0].price, expected_offer)
-        # check volumes
+        self.assertEqual(exchange.buy_order_book()[0].quantity, mm.ORDER_QUANTITY)
+        self.assertEqual(exchange.sell_order_book()[0].quantity, mm.ORDER_QUANTITY)
         
         
     # check that when a trade is completed the market maker trades are replaced
